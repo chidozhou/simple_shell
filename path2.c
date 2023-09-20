cf5c2fa7c16afac76bd873a81896594833e789d3
chidozhou <chidochashegerald@gmail.com>
2023-09-20 04:26:00 -0700

    path2

--- path2.c
+++ path2.c
@@ -8,45 +8,45 @@
  */
 void mypath_check(vars_t *myvars)
 {
-	char *the_path = NULL;
-	char **token_path = NULL;
-	char *checker = NULL;
-	struct stat buff;
-	int found = 0;
-	unsigned int j;
+	char *path;
+	char **paths;
+	int i;
 
-	if (mydir_check(myvars->av[0])
-		{
-			mycwd_exe(myvars);
-			return;
-		}
+	if (mydir_check(myvars->av[0]))
+	{
+		mycwd_exe(myvars);
+		return;
+	}
 
-		the_path = mypath_find(myvars->env);
-		if (the_path == NULL)
-		{
-			myerror_print(myvars, ": not found\n");
-			myvars->status = 127;
-			my_exit(myvars);
-		}
+	path = mypath_find(myvars->env);
+	if (path == NULL)
+	{
+		myerror_print(myvars, ": PATH environment variable is not set\n");
+		myvars->status = 127;
+		my_exit(myvars);
+	}
 
-		token_path = my_tkenizer(the_path + 5, ":");
-		for (j = 0; token_path[j]; j++)
+	paths = my_tkenizer(path, ":");
+	for (i = 0; paths && paths[i]; i++)
+	{
+		char *full_path = my_strcat(paths[i], myvars->av[0]);
+
+		struct stat buff;
+
+		if (stat(full_path, &buff) == 0)
 		{
-			checker = my_strcat(token_path[j], myvars->av[0]);
-			if (stat(checker, &buff) == 0)
-			{
-				found = 1;
-				break;
-			}
-			free(checker);
+			mypath_exe(full_path, myvars);
+			free(full_path);
+			break;
 		}
-	free(token_path);
-	if (!found)
+		free(full_path);
+	}
+	free(paths);
+	if (paths[i] == NULL)
 	{
 		myerror_print(myvars, ": not found\n");
 		myvars->status = 127;
 		my_exit(myvars);
 	}
-	mypath_exe(checker, myvars);
 }
 
