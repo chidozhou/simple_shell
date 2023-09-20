87a331a9574a7bc68faf42f3fccdcecbd324b7fe
chidozhou <chidochashegerald@gmail.com>
2023-09-20 13:04:44 -0700

    updated buitlins02.c

--- builtins02.c
+++ builtins02.c
@@ -1,19 +1,30 @@
 #include "shell.h"
+
 /**
- * my_env - prints the current environment
- * @myvars: struct of variables
+ * my_env - Print the current environment variables.
+ * @myvars: A struct containing environment data.
  *
- * Return: void.
+ * Description:
+ * This function prints the current environment stndard output.
+ *
+ * Return: void
  */
 void my_env(vars_t *myvars)
 {
-	unsigned int j;
+	/* Initialize a counter for iterating through the environment */
+	unsigned int j = 0;
 
-	for (j = 0; myvars->env[j]; j++)
+	/* Loop until reaching the end of the environment array */
+	while (myvars->env[j])
 	{
+		/* Print the current environment variable */
 		my_puts(myvars->env[j]);
+
+		/* Print a newline character to separate variables */
 		my_puts("\n");
+		j++;
 	}
+	/* Set the status to indicate success */
 	myvars->status = 0;
 }
 
