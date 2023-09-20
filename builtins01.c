45e2135bd18dad8bcd80ea72a7a32e2661a1ed99
chidozhou <chidochashegerald@gmail.com>
2023-09-20 12:04:08 -0700

    updated bultins01.c

--- builtins01.c
+++ builtins01.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
  * my_exit - Exit the program with a specified status code
  * @myvars: Variables containing command and status
@@ -9,9 +10,13 @@ void my_exit(vars_t *myvars)
 {
 	int exit_status;
 
-	if (my_strcmpr(myvars->av[0], "exit") == 0 && myvars->av[1] != NULL)
+	/* Check if the command is "exit" and if an exit status is provided */
+	while (my_strcmpr(myvars->av[0], "exit") == 0 && myvars->av[1] != NULL)
 	{
+		/* Convert the exit status argument to an integer */
 		exit_status = my_atoi(myvars->av[1]);
+
+		/* If the exit status is -1 (invalid) 2 and display an error msg */
 		if (exit_status == -1)
 		{
 			myvars->status = 2;
@@ -22,8 +27,13 @@ void my_exit(vars_t *myvars)
 			myvars->commands = NULL;
 			return;
 		}
+
+		/* Set the program's exit status to the specified value and break the loop */
 		myvars->status = exit_status;
+		break;
 	}
+
+	/* Free allocated memory and exit the program */
 	free(myvars->buffer);
 	free(myvars->av);
 	free(myvars->commands);
